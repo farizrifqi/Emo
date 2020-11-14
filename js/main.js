e5dffe3d15b06020be41a51e03619c0a506aeb49
Fariz Rifqi <farizrifqi26@gmail.com>
2020-11-14 23:02:59 +0800

    edit beberapa typo

--- js/main.js
+++ js/main.js
@@ -53,6 +53,9 @@ function switchPage(p){
 		case "deposit":
 			document.getElementById("main-content").innerHTML='<object type="text/html" width="100%" height="100%" data="page/deposit.html" ></object>';
 		break;
+		case "withdraw":
+			document.getElementById("main-content").innerHTML='<object type="text/html" width="100%" height="100%" data="page/withdraw.html" ></object>';
+		break;
 		default:
 			document.getElementById("main-content").innerHTML='<object type="text/html" width="100%" height="100%" data="page/beranda.html" ></object>';
 		break;
