84cd6810c068a02eb80d654b74725f987bc878a0
Fariz Rifqi <farizrifqi26@gmail.com>
2020-12-07 16:02:12 +0800

    edit

--- login.php
+++ login.php
@@ -1,3 +1,7 @@
+<?php
+include 'lib/koneksi.php';
+session_start();
+?>
 <html>
 <head>
 	<!-- Meta -->
@@ -9,12 +13,29 @@
 </head>
 <body class="primary">
 	<div class="box">
+	<?php
+	if (isset($_POST['login'])){
+		if($con->query("SELECT * FROM user where email='".$_POST['email']."'")->num_rows > 0){
+			$get = $con->query("SELECT email, nama_depan, password FROM user where email='".$_POST['email']."' LIMIT 1")->fetch_assoc();
+			if ($get['password'] != $_POST['password']){
+				echo "<center><br/><font color=\"red\">Email atau password salah</font><br/><br/></center>";
+			}else{
+				$_SESSION['email'] = $get['email'];
+				$_SESSION['namadepan'] = $get['nama_depan'];
+				header('location: dashboard.php');
+			}
+		}else{
+			echo "<center><br/><font color=\"red\">Email atau password salah</font><br/><br/></center>";
+		}
+		
+	}
+	?>
 	<form action="" method="POST">
 		<label for="email">Email</label><br/>
 		<input type="text" id="login-email" name="email" class="input-text" placeholder="Login email" required><br/><br/>
 		<label for="password">Password</label><br/>
-		<input type="password" id="login-pass" name="password" class="input-text" autocomplete="off" placeholder="Login password" required><br/></br>
-		<input type="submit" class="btn btn-masuk" autocomplete="off" value="Login"/> 
+		<input type="password" id="login-pass" name="password" class="input-text" autocomplete="off" placeholder="Login password" name="pw" required><br/></br>
+		<input type="submit" class="btn btn-masuk" name="login" autocomplete="off" value="Login"/> 
 	</form>
 	<a href="#"><button class="btn btn-fp" value="Lupa password">Lupa password</button></a>
 	<p>Belum punya akun? <a href="daftar.html">Daftar</a> yuk!</p>
@@ -29,6 +50,7 @@
 		</div>
 	</div>
 	</footer>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 	<script src="js/app.js" type="text/javascript"></script>
 </body>
 </html>
