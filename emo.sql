798a9a4e7b53e3d3e3c3622187ac5ebe88ade102
Fariz Rifqi <farizrifqi26@gmail.com>
2020-12-08 23:38:14 +0800

    update sql

--- emo.sql
+++ emo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 07 Des 2020 pada 09.56
+-- Waktu pembuatan: 08 Des 2020 pada 16.36
 -- Versi server: 10.1.37-MariaDB
 -- Versi PHP: 7.3.0
 
@@ -21,6 +21,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `emo`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `riwayat_harga`
+--
+
+CREATE TABLE `riwayat_harga` (
+  `id` int(11) NOT NULL,
+  `harga` int(255) NOT NULL,
+  `waktu` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `riwayat_harga`
+--
+
+INSERT INTO `riwayat_harga` (`id`, `harga`, `waktu`) VALUES
+(1, 953000, '2020-12-08 12:28:11'),
+(2, 966000, '2020-12-08 12:55:33');
 
 -- --------------------------------------------------------
 
@@ -43,7 +63,34 @@ CREATE TABLE `riwayat_login` (
 INSERT INTO `riwayat_login` (`id`, `id_user`, `ua`, `ip`, `time`) VALUES
 (1, 1, 'Mozilla/5.0 (Windows NT 10.0; ', '::1', '2020-12-07 08:23:37'),
 (2, 2, 'Mozilla/5.0 (Windows NT 10.0; ', '::1', '2020-12-07 08:36:29'),
-(3, 1, 'Mozilla/5.0 (Windows NT 10.0; ', '::1', '2020-12-07 08:38:16');
+(3, 1, 'Mozilla/5.0 (Windows NT 10.0; ', '::1', '2020-12-07 08:38:16'),
+(4, 6, 'Mozilla/5.0 (Windows NT 10.0; ', '::1', '2020-12-08 11:21:45'),
+(5, 1, 'Mozilla/5.0 (Windows NT 10.0; ', '::1', '2020-12-08 11:33:41'),
+(6, 1, 'Mozilla/5.0 (Windows NT 10.0; ', '::1', '2020-12-08 12:44:45'),
+(7, 1, 'Mozilla/5.0 (Windows NT 10.0; ', '::1', '2020-12-08 15:09:22');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(6) NOT NULL,
+  `id_user` int(6) NOT NULL,
+  `tipe` varchar(30) NOT NULL,
+  `deskripsi` varchar(30) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `id_user`, `tipe`, `deskripsi`, `jumlah`, `time`) VALUES
+(1, 1, 'Deposit', 'via BCA VA', 10000000, '2020-12-08 12:05:38'),
+(2, 1, 'Deposit', 'via BCA VA', 100000, '2020-12-08 12:07:13');
 
 -- --------------------------------------------------------
 
@@ -54,7 +101,7 @@ INSERT INTO `riwayat_login` (`id`, `id_user`, `ua`, `ip`, `time`) VALUES
 CREATE TABLE `user` (
   `id` int(6) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `nama_depan` varchar(30) NOT NULL,
   `nama_belakang` varchar(30) NOT NULL,
   `no_hp` varchar(15) NOT NULL,
@@ -69,20 +116,33 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `password`, `nama_depan`, `nama_belakang`, `no_hp`, `tanggal_lahir`, `alamat`, `register_date`, `last_login`) VALUES
-(1, 'farizrifqi26@gmail.com', 'asdf', 'Fariz', 'Rifqi', 'Fariz', '2020-11-07', 'asdf', '2020-12-07', '2020-12-07 15:30:46'),
-(2, 'refrira@gmail.com', '123123', 'Refri', 'Rizky', '085155094006', '2020-11-07', 'asdf', '2020-12-07', '2020-12-07 15:38:06'),
-(3, 'zeranel@gmail.com', 'Bismillah123', 'Zere', 'Nale', '082190450269', '2020-11-07', 'Jl Kaliurang', '2020-12-07', '2020-12-07 15:43:49'),
-(4, 'agastian@gmail.com', 'agas123123', 'Agastian', 'Rizki', '085155094006', '2020-11-07', 'Jl Concat', '2020-12-07', '2020-12-07 15:46:30'),
-(5, 'rafiq@gmail.com', '123123123', 'Rafiq', 'Ezza', '082142312324', '2020-11-07', 'sadfasdf', '2020-12-07', '2020-12-07 15:47:30');
+(1, 'farizrifqi26@gmail.com', 'OTBUVVpwblV0cEZlSnBYVA==', 'Fariz', 'Rifqi', 'Fariz', '2020-11-07', 'asdf', '2020-12-07', '2020-12-08 23:09:22'),
+(2, 'refrira@gmail.com', 'PT1RUDljMllzcFZiakJuU1lsRmVKcFhU', 'Refri', 'Rizky', '085155094006', '2020-11-07', 'asdf', '2020-12-07', '2020-12-07 15:38:06'),
+(3, 'zeranel@gmail.com', 'PT1RUDljV1pzcEVXWlZuVkhKR2VKcFhU', 'Zere', 'Nale', '082190450269', '2020-11-07', 'Jl Kaliurang', '2020-12-07', '2020-12-07 15:43:49'),
+(4, 'agastian@gmail.com', 'PT1RUDlFVld1WmtNamhYUzYxRWVKcFhU', 'Agastian', 'Rizki', '085155094006', '2020-11-07', 'Jl Concat', '2020-12-07', '2020-12-07 15:46:30'),
+(5, 'rafiq@gmail.com', 'NGxrZU5oWFM2MUVlSnBYVA==', 'Rafiq', 'Ezza', '082142312324', '2020-11-07', 'sadfasdf', '2020-12-07', '2020-12-07 15:47:30'),
+(6, 'ansellma@gmail.com', 'NGxrZU5oWFM2MUVlSnBYVA==', 'Ansellma', 'Putri', '082190450269', '2020-11-08', 'Jl Kaliurang', '2020-12-08', '2020-12-08 19:19:00');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indeks untuk tabel `riwayat_harga`
+--
+ALTER TABLE `riwayat_harga`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `riwayat_login`
 --
 ALTER TABLE `riwayat_login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -96,16 +156,28 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `riwayat_harga`
+--
+ALTER TABLE `riwayat_harga`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT untuk tabel `riwayat_login`
 --
 ALTER TABLE `riwayat_login`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
